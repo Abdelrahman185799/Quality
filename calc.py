19fa8a7ad43a059220525fa9746f2871106ae302
Abdelrahman <Abdelrahman.tarek8@msa.edu.eg>
2022-01-07 17:19:40 +0200

    adjusting multiply equation

--- calc.py
+++ calc.py
@@ -39,7 +39,7 @@ while should_continue:
     if operation_symbol == "-":
         answer = subtract(num1, num2)
     if operation_symbol == "*":
-        answer = multiply(num1, num1)
+        answer = multiply(num1, num2)
     if operation_symbol == "/":
         answer = divide(num1, num2)
     print(f"{num1} {operation_symbol} {num2} = {answer}")
